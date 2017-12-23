730e166172a13aaf80a9d6571ad1301a47a57bda
SciFi1818 <sajalsanthosh81@gmail.com>
2017-12-23 19:20:42 +0530

    .

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Upload travis generated APKs to the Transfer.sh
-cd /home/travis/build/SciFi1818/VirtualHookEx/app/lib/build/outputs
+cd /home/travis/build/SciFi1818/SciFi-Hook/app/lib/build/outputs
 ls
 zip -r virtualhook.zip apk *
 cu=`curl --upload-file virtualhook.zip https://transfer.sh/virtualhook.zip`
